c0a2cafe97015268c79a52c8dd98290d16ff3288
Yuktha mukhi <164024317+yuktham357@users.noreply.github.com>
2025-02-20 23:08:45 -0800

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of digits in n that evenly divide n
+    int evenlyDivides(int n) {
+        int count=0;
+        int r=n;
+        
+        while(n>0){
+            int rem=n%10;
+            if(rem!=0&&r%rem==0){
+                count++;
+          
+            }
+            n=n/10;
+        }
+        return count;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
