0cb7b013a10d59f4cc4f0110f81bd0bbfe845e48
Yuktha mukhi <164024317+yuktham357@users.noreply.github.com>
2025-06-10 23:56:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int countStrings(string &s) {
+        // code here
+         int count=0,l=s.length();
+        unordered_map<char,int> u;
+        bool flag=false;
+        for(char i:s)
+        {
+            u[i]++;
+        }
+        for(char ch:s)
+        {
+            if(u[ch]>1)
+            flag=true;
+            count+=l-u[ch];
+        }
+        count/=2;
+        if(flag==true)
+        count++;
+        return count;
+    }
+};
